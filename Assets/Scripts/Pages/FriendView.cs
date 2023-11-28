1b7edf07b78c15988604d578fcde6acc73b87b5d
TaMi <gary321588@gmail.com>
2023-11-28 15:34:43 +0800

    ALL Normal System Done

--- Assets/Scripts/Pages/FriendView.cs
+++ Assets/Scripts/Pages/FriendView.cs
@@ -1,10 +1,27 @@
-﻿namespace AIExhibition.UIManagement.Base.Pages
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AIExhibition.UIManagement.Base.Pages
 {
     public class FriendView:View
     {
+        [SerializeField] private Button backbutton;
+        [SerializeField] private View mainView;
         public override void Initialize()
         {
+            backbutton.onClick.AddListener(GoBack);
             Show();
+        }
+
+        private void OnDestroy()
+        {
+            backbutton.onClick.RemoveAllListeners();
+        }
+
+        void GoBack()
+        {
+            mainView.Initialize();
+            Hide();
         }
     }
 }
